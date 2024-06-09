b234c966639337ff7ef3e7c34a5f70f0fa4e7713
Nodar Mosia <nodarmosia@gmail.com>
2024-06-09 22:18:52 +0400

    caching month old prog

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/tursodatabase/go-libsql"
+)
+
 func main() {
-	println("Hello, World!")
+	dbName := "local.db"
+	primaryUrl := "libsql://[DATABASE].turso.io"
+	authToken := "..."
+
+	dir, err := os.MkdirTemp("", "libsql-embedded")
+	if err != nil {
+		fmt.Println("Error creating temporary directory:", err)
+		os.Exit(1)
+	}
+	defer os.RemoveAll(dir)
+
+	dbPath := filepath.Join(dir, dbName)
+
+	connector, err := libsql.NewEmbeddedReplicaConnector(dbPath, primaryUrl,
+		libsql.WithAuthToken(authToken),
+	)
+	if err != nil {
+		fmt.Println("Error creating connector:", err)
+		os.Exit(1)
+	}
+	defer connector.Close()
+
+	db := sql.OpenDB(connector)
+	defer db.Close()
 }
